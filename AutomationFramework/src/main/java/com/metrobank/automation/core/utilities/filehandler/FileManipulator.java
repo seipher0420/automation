51d9f19d2a359a62e19f48c4f34be4fcf0c3491a
seipher0420 <phage999@gmail.com>
2020-04-06 10:53:55 +0800

    Update FileManipulator

--- AutomationFramework/src/main/java/com/metrobank/automation/core/utilities/filehandler/FileManipulator.java
+++ AutomationFramework/src/main/java/com/metrobank/automation/core/utilities/filehandler/FileManipulator.java
@@ -2,9 +2,12 @@ package main.java.com.metrobank.automation.core.utilities.filehandler;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 import main.java.com.metrobank.automation.generics.AutomationConstants;
 
@@ -39,23 +42,21 @@ public class FileManipulator {
 	}
 	
 	public String readPdfFile(String filePath) throws IOException{
-		File file = new File(filePath);
-		String text = null;
-		PDDocument pdfFile = null;
-		PDFTextStripper pdfStripper = new PDFTextStripper();
-		
-		if(file.exists()){
-			 pdfFile = PDDocument.load(file);
-			 if(!pdfFile.isEncrypted()){
-					text = pdfStripper.getText(pdfFile);
-					System.out.println(AutomationConstants.FRAMEWORK_LOGS + "Successfully Read PDF File.");
+		StringBuilder text =  new StringBuilder();
+		try (PDDocument document = PDDocument.load(new File(filePath))) {
+			document.getClass();
+			if (!document.isEncrypted()) {
+				PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+				stripper.setSortByPosition(true);
+				PDFTextStripper tStripper = new PDFTextStripper();
+				String pdfFileInText = tStripper.getText(document);
+				String lines[] = pdfFileInText.split("\\r?\\n");
+				for (String line : lines) {
+					text.append(line);
 				}
-		}else{
-			System.out.println(AutomationConstants.FRAMEWORK_LOGS + "Failed to read PDF File not Found.");
+			}
 		}
-		
-		pdfFile.close();
-		return text;	
+		return text.toString();
 	}
 	
 
